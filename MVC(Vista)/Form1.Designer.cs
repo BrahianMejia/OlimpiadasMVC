115811b915a82640635e2dbbd5a6374a6be988ff
Brahian Mejia <brayantqm123@gmail.com>
2019-11-03 13:55:21 -0500

    Subida del proyecto de las olimpiadas donde se implementa el uso de MVC.

---
+++ MVC(Vista)/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace MVC_Vista_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpaises = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnombrepais = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.btnguardarcambiospais = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.btncancelar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.idpais = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombrepais = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtpaises)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtpaises
+            // 
+            this.dtpaises.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtpaises.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idpais,
+            this.nombrepais});
+            this.dtpaises.Location = new System.Drawing.Point(51, 286);
+            this.dtpaises.Name = "dtpaises";
+            this.dtpaises.Size = new System.Drawing.Size(321, 150);
+            this.dtpaises.TabIndex = 0;
+            this.dtpaises.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtpaises_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Paises:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gestión Países";
+            // 
+            // txtnombrepais
+            // 
+            this.txtnombrepais.Location = new System.Drawing.Point(159, 124);
+            this.txtnombrepais.MaxLength = 50;
+            this.txtnombrepais.Name = "txtnombrepais";
+            this.txtnombrepais.Size = new System.Drawing.Size(209, 20);
+            this.txtnombrepais.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(51, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nombre país:";
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Location = new System.Drawing.Point(55, 170);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(63, 32);
+            this.btnguardar.TabIndex = 5;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // btnguardarcambiospais
+            // 
+            this.btnguardarcambiospais.Location = new System.Drawing.Point(124, 170);
+            this.btnguardarcambiospais.Name = "btnguardarcambiospais";
+            this.btnguardarcambiospais.Size = new System.Drawing.Size(109, 32);
+            this.btnguardarcambiospais.TabIndex = 6;
+            this.btnguardarcambiospais.Text = "Guardar Cambios";
+            this.btnguardarcambiospais.UseVisualStyleBackColor = true;
+            this.btnguardarcambiospais.Click += new System.EventHandler(this.btnguardarcambiospais_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(239, 170);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(66, 32);
+            this.btneliminar.TabIndex = 7;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.Location = new System.Drawing.Point(311, 170);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(61, 32);
+            this.btncancelar.TabIndex = 8;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.UseVisualStyleBackColor = true;
+            this.btncancelar.Click += new System.EventHandler(this.btncancelar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(51, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Código:";
+            this.label4.Visible = false;
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(159, 85);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(74, 20);
+            this.txtcodigo.TabIndex = 10;
+            this.txtcodigo.Visible = false;
+            // 
+            // idpais
+            // 
+            this.idpais.DataPropertyName = "idpais";
+            this.idpais.HeaderText = "Código";
+            this.idpais.Name = "idpais";
+            // 
+            // nombrepais
+            // 
+            this.nombrepais.DataPropertyName = "nombrepais";
+            this.nombrepais.HeaderText = "Nombre País";
+            this.nombrepais.Name = "nombrepais";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 450);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnguardarcambiospais);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnombrepais);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpaises);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtpaises)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtpaises;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnombrepais;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Button btnguardarcambiospais;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btncancelar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idpais;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombrepais;
+    }
+}
+
---
+++ MVC(Vista)/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Controlador.Paises;
+
+namespace MVC_Vista_
+{
+    public partial class Form1 : Form
+    {
+
+        PaisesDTO paisesDTO = null;
+        PaisesDAO paisesDAO = null;
+        DataTable Dtt = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ListarPaises();
+            btneliminar.Enabled = false;
+            btnguardarcambiospais.Enabled = false;
+            btncancelar.Enabled = false;
+        }
+
+        public void ListarPaises()
+        {
+
+            paisesDTO = new PaisesDTO();
+
+            paisesDAO = new PaisesDAO(paisesDTO);
+
+            Dtt = new DataTable();
+            Dtt = paisesDAO.ListarPaises();
+
+            if (Dtt.Rows.Count > 0)
+            {
+                dtpaises.DataSource = Dtt;
+            }
+            else
+            {
+                MessageBox.Show("No hay registros de Paises");
+            }
+        }
+
+        public void Guardar()
+        {
+            paisesDTO = new PaisesDTO();
+            paisesDTO.setNombrepais(txtnombrepais.Text);
+            paisesDAO = new PaisesDAO(paisesDTO);
+
+            paisesDAO.GuardarPais();
+            MessageBox.Show("Registro guardado", "Mensaje");
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            if (txtnombrepais.Text.Trim() == "")
+            {
+                MessageBox.Show("Intenta un dato válido.", "Mensaje");
+                txtnombrepais.Focus();
+            }
+            else
+            {
+                Guardar();
+                ListarPaises();
+                txtnombrepais.Clear();
+                txtnombrepais.Focus();
+            }
+        }
+
+        private void dtpaises_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            label4.Visible = true;
+            txtcodigo.Visible = true;
+
+            txtcodigo.Text = dtpaises.Rows[dtpaises.CurrentRow.Index].Cells[0].Value.ToString();
+            txtnombrepais.Text = dtpaises.Rows[dtpaises.CurrentRow.Index].Cells[1].Value.ToString();
+
+            btnguardar.Enabled = false;
+            txtcodigo.Enabled = false;
+            btnguardarcambiospais.Enabled = true;
+            btncancelar.Enabled = true;
+            btneliminar.Enabled = true;
+        }
+
+        public void GuardarCambios()
+        {
+            paisesDTO = new PaisesDTO();
+            paisesDTO.setIdpais(Convert.ToInt16(txtcodigo.Text));
+            paisesDTO.setNombrepais(txtnombrepais.Text);
+            paisesDAO = new PaisesDAO(paisesDTO);
+
+            paisesDAO.GuardarCambiosPais();
+            MessageBox.Show("Registro actualizado", "Mensaje");
+        }
+
+        private void btnguardarcambiospais_Click(object sender, EventArgs e)
+        {
+
+            if (txtnombrepais.Text.Trim() == "")
+            {
+                MessageBox.Show("Intenta un dato válido", "Mensaje");
+                txtnombrepais.Focus();
+            }
+            else
+            {
+                GuardarCambios();
+                ListarPaises();
+
+                label4.Visible = false;
+                txtcodigo.Visible = false;
+                btnguardar.Enabled = true;
+                btnguardarcambiospais.Enabled = false;
+                btneliminar.Enabled = false;
+                btncancelar.Enabled = false;
+                txtnombrepais.Clear();
+                txtnombrepais.Focus();
+            }
+            
+        }
+
+        public void EliminarRegistroPais()
+        {
+            paisesDTO = new PaisesDTO();
+            paisesDTO.setIdpais(Convert.ToInt16(txtcodigo.Text));
+            paisesDAO = new PaisesDAO(paisesDTO);
+
+            paisesDAO.EliminarPais();
+            MessageBox.Show("Registro eliminado", "Mensaje");
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            EliminarRegistroPais();
+            ListarPaises();
+            btnguardar.Enabled = true;
+            btnguardarcambiospais.Enabled = true;
+            btneliminar.Enabled = true;
+            btncancelar.Enabled = true;
+
+        }
+
+        private void btncancelar_Click(object sender, EventArgs e)
+        {
+            ListarPaises();
+
+            label4.Visible = false;
+            txtcodigo.Visible = false;
+            btnguardar.Enabled = true;
+            btnguardarcambiospais.Enabled = false;
+            btneliminar.Enabled = false;
+            btncancelar.Enabled = false;
+            txtnombrepais.Clear();
+            txtnombrepais.Focus();
+        }
+    }
+}
